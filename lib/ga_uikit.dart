883000b45e9ddbd5fcf591faed7c73f1a6675787
gataman <gurcanataman@msn.com>
2023-02-15 21:01:41 +0300

    menu item added

--- lib/ga_uikit.dart
+++ lib/ga_uikit.dart
@@ -1,5 +1,9 @@
 library ga_uikit;
 
 export 'src/widgets/tab_bar/ga_tab_bar.dart';
+
 export 'src/widgets/drop_down/ga_drop_down.dart';
+export 'src/widgets/drop_down/ga_drop_down_with_menu.dart';
+export 'src/widgets/drop_down/menu_item.dart';
+
 export 'src/widgets/popup_menu/ga_popup_menu_button.dart';
