ed65140b2de8796d5b75e45483c456ae8defb563
gataman <gurcanataman@msn.com>
2023-04-18 12:15:30 +0300

    changed dialog size

---
+++ lib/src/constants/dialog_constants.dart
@@ -0,0 +1,8 @@
+class DialogConstants {
+  const DialogConstants._();
+
+  static const double smallWidthPadding = 20;
+  static const double mediumWidthPadding = 50;
+  static const double largeWidthPadding = 250;
+  static const double heightPadding = 50;
+}
--- lib/src/widgets/dialog/ga_form_dialog.dart
+++ lib/src/widgets/dialog/ga_form_dialog.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
+import 'package:ga_uikit/src/constants/dialog_constants.dart';
 
 import '../../enums/icon_position.dart';
 import '../../enums/radius_type.dart';
@@ -8,16 +10,28 @@ import '../container/ga_rounded_container.dart';
 part 'ga_form_dialog_content.dart';
 
 class GaFormDialog extends StatelessWidget {
-  const GaFormDialog({
-    super.key,
-    required this.buildContent,
-    this.widthRatio = .5,
-    this.heightRatio = .8,
-  });
+  const GaFormDialog(
+      {super.key,
+      required this.buildContent,
+      @Deprecated('Use largePadding, mediumPadding and smallPadding. '
+          'You can use it to set the dialog size')
+          double? widthRatio = .5,
+      @Deprecated('Use largePadding, mediumPadding and smallPadding. '
+          'You can use it to set the dialog size')
+          double? heightRatio = .8,
+      this.largePadding,
+      this.mediumPadding,
+      this.smallPadding,
+      this.dialogRadius});
 
-  // This only works in desktop size
-  final double widthRatio;
-  final double heightRatio;
+  final EdgeInsets? largePadding;
+
+  final EdgeInsets? mediumPadding;
+
+  final EdgeInsets? smallPadding;
+
+  final BorderRadiusGeometry? dialogRadius;
+
   final Widget Function(BuildContext context) buildContent;
 
   @override
@@ -27,34 +41,76 @@ class GaFormDialog extends StatelessWidget {
         builder: (context) {
           return Scaffold(
             backgroundColor: Colors.transparent,
-            body: Dialog(
-                insetPadding: EdgeInsets.zero,
-                shape: RoundedRectangleBorder(borderRadius: RadiusType.all.getRadius(10.0)),
-                child: LayoutBuilder(
-                  builder: (context, boxConstraints) {
-                    final width = _getDialogWidth(boxConstraints);
-                    final height = _getDialogHeight(boxConstraints);
-                    return SizedBox(
-                      width: width,
-                      height: height,
-                      child: buildContent(context),
+            body: AdaptiveLayout(
+              body: SlotLayout(config: <Breakpoint, SlotLayoutConfig>{
+                const WidthPlatformBreakpoint(begin: 1000): SlotLayout.from(
+                  key: const Key('dialog_large'),
+                  builder: (context) {
+                    return _getDialog(
+                      context: context,
+                      insetPadding: largePadding ??
+                          const EdgeInsets.symmetric(
+                            horizontal: DialogConstants.largeWidthPadding,
+                            vertical: DialogConstants.heightPadding,
+                          ),
                     );
                   },
-                )),
+                ),
+                const WidthPlatformBreakpoint(begin: 450, end: 999): SlotLayout.from(
+                  key: const Key('dialog_medium'),
+                  builder: (context) {
+                    return _getDialog(
+                      context: context,
+                      insetPadding: mediumPadding ??
+                          const EdgeInsets.symmetric(
+                            horizontal: DialogConstants.mediumWidthPadding,
+                            vertical: DialogConstants.heightPadding,
+                          ),
+                    );
+                  },
+                ),
+                const WidthPlatformBreakpoint(begin: 0, end: 449): SlotLayout.from(
+                  key: const Key('dialog_small'),
+                  builder: (context) {
+                    debugPrint('small and up');
+                    return _getDialog(
+                      context: context,
+                      insetPadding: mediumPadding ??
+                          const EdgeInsets.symmetric(
+                            horizontal: DialogConstants.smallWidthPadding,
+                            vertical: DialogConstants.heightPadding,
+                          ),
+                    );
+                  },
+                ),
+              }),
+            ),
           );
         },
       ),
     );
   }
 
-  double _getDialogWidth(BoxConstraints boxConstraints) =>
-      boxConstraints.maxWidth > 1000 ? boxConstraints.maxWidth * widthRatio : boxConstraints.maxWidth * .9;
-
-  double _getDialogHeight(BoxConstraints boxConstraints) => boxConstraints.maxHeight * heightRatio;
+  Dialog _getDialog({required BuildContext context, EdgeInsets? insetPadding}) {
+    return Dialog(
+        insetPadding: insetPadding,
+        shape: RoundedRectangleBorder(borderRadius: dialogRadius ?? RadiusType.all.getRadius(10.0)),
+        child: buildContent(context));
+  }
 }
 
 
 /*!SECTION
-
+LayoutBuilder(
+                builder: (context, boxConstraints) {
+                  final width = _getDialogWidth(boxConstraints);
+                  final height = _getDialogHeight(boxConstraints);
+                  return SizedBox(
+                    width: width,
+                    height: height,
+                    child: buildContent(context),
+                  );
+                },
+              ),
 
 */
